7bbbb60e9f84eaf44b45e619efcd846bd20b5506
A01173474 <A01173474@tec.mx>
2023-02-04 22:45:29 -0600

    rutas

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,7 +1,7 @@
 const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
-//const Task = require('../model/task');
+const Task = require('../model/task');
 let router = express()
 let Task = require("../model/task")
 let User = require("../model/user")
